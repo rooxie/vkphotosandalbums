2f30c8c6d4d77d8e10ef499453db06e73c17c90c
Roman Derlemenko <romanderlemenko@gmail.com>
2017-07-03 10:56:10 +0300

    Users output simplified

--- src/VkPhotosAndAlbums/Commands/GetUsersCommand.php
+++ src/VkPhotosAndAlbums/Commands/GetUsersCommand.php
@@ -21,15 +21,10 @@ class GetUsersCommand extends BaseCommand
         $this->output->writeln("Found {$count} users in the database");
 
         foreach ($users as $user) {
-            $this->outputUser($user);
+            $this->output->writeln('');
+            $this->output->writeln('<info>Id: </info>'          . $user->getId());
+            $this->output->writeln('<info>First Name: </info>'  . $user->getFirstName());
+            $this->output->writeln('<info>Last Name: </info>'   . $user->getLastName());
         }
-    }
-
-    protected function outputUser(Users $user): void
-    {
-        $this->output->writeln(PHP_EOL);
-        $this->output->writeln('<info>Id: </info>' . $user->getId());
-        $this->output->writeln('<info>First Name: </info>' . $user->getFirstName());
-        $this->output->writeln('<info>Last Name: </info>' . $user->getLastName());
     }
 }
